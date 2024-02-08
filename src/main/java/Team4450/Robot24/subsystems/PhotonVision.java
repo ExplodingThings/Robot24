98aaaecf155ed8983227b732e57e353eb3ed3011
ExplodingThings <apalka34@gmail.com>
2024-02-07 17:02:46 -0800

    fixed error

--- src/main/java/Team4450/Robot24/subsystems/PhotonVision.java
+++ src/main/java/Team4450/Robot24/subsystems/PhotonVision.java
@@ -19,6 +19,7 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
